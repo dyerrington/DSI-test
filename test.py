45185a6397e93f4248312f021ec556674097f082
David Yerrington <david@yerrington.net>
2016-09-26 10:48:57 -0700

    who needs a test file anywway?

--- test.py
+++
@@ -1,9 +0,0 @@
-echo "this is some good code yo"
-
-if this_Var:
-    print "har haw haw har! There will be a merge conflict! Mwah hahahaha!" 
-    print "there will be more conflicts on my watch!! HWA!"
-    # This is an important message concerning our code
-    # This blablablablablabl
-    # Anothing line
-
