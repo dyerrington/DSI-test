8ee414b33c6046b1be54f52eda26bff8e9819169
Alex Halcomb <alexhalcomb1@gmail.com>
2016-09-26 11:56:01 -0700

    push it

--- alex_h.py
+++ alex_h.py
@@ -4,4 +4,5 @@ if type(alex) == "person":
     print "this is a person object"
 
 if type(alex) == "java_object":
-    print "this would never happen in python"
+    print "this would never happen in python, because python rulez"
+
