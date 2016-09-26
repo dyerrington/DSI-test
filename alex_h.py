1eb0d761c116684c8d2443f64842e8faf19bfb36
David Yerrington <david@yerrington.net>
2016-09-26 11:52:32 -0700

    new file for alex

---
+++ alex_h.py
@@ -0,0 +1,7 @@
+print "This is the file for alex"
+
+if type(alex) == "person":
+    print "this is a person object"
+
+if type(alex) == "java_object":
+    print "this would never happen in python"
